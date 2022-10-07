8cffc6a6d089005da7609381e17896ab6ef934b9
ljs <qazplm807733@gmail.com>
2022-10-07 22:32:58 +0900

    add index.tsx UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
 import type { NextPage } from "next";
-import { useState } from "react";
+import Layout from "../components/Layout";
 
 const Home: NextPage = () => {
-  return <div className="bg-red-200 w-full h-[100vh]">1</div>;
+  return (
+    <div>
+      <Layout></Layout>
+      <div className="bg-slate-700 w-full h-[100vh] flex justify-evenly items-center">
+        <div className="bg-red-600 w-[50%] h-[70%]">Chart 들어갈거임</div>
+        <div className="w-[30%] h-[70%] flex justify-center items-end">
+          <button className="bg-blue-600 w-[30%] h-[10%] rounded-lg text-2xl font-mono font-extrabold text-white mb-8">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ pages/mypage.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import Layout from "../components/Layout";
+
+export default function MyPage() {
+  const [stat, setStat] = useState(0);
+
+  console.log(stat);
+
+  return (
+    <div>
+      <Layout></Layout>
+      <div className="bg-slate-700 w-full h-[100vh] flex space-x-6 justify-center items-center">
+        <div className="h-[80%] w-[15%] border-2 rounded-2xl flex text-white font-bold text-lg flex-col justify-around">
+          <div className="flex justify-center text-3xl">My Page</div>
+          <button className="text-2xl" onClick={() => setStat(1)}>
+            회원 정보
+          </button>
+          <button className="text-2xl" onClick={() => setStat(2)}>
+            이용 내역
+          </button>
+          <button className="text-2xl" onClick={() => setStat(3)}>
+            서비스 신청
+          </button>
+          <button className="text-2xl" onClick={() => setStat(4)}>
+            로그아웃
+          </button>
+        </div>
+        <div className="w-[75%] h-[80%] border-2 rounded-2xl flex justify-center items-center">
+          {stat === 1 && (
+            <div className="flex flex-col w-[90%] h-[90%] items-center space-y-32">
+              <div className="text-4xl font-mono font-bold text-white">
+                회원정보
+              </div>
+              <div className="w-[70%] h-[50%] flex flex-col justify-around">
+                <label className="text-3xl font-mono font-bold text-white">
+                  Email
+                </label>
+                <div className="text-2xl font-mono font-bold text-white">
+                  이메일 나올 곳
+                </div>
+                <label className="text-3xl font-mono font-bold text-white">
+                  NickName
+                </label>
+                <div className="text-2xl font-mono font-bold text-white">
+                  닉넴 나올 곳
+                </div>
+              </div>
+            </div>
+          )}
+          {stat === 2 && (
+            <div className="flex flex-col w-[90%] h-[90%] items-center space-y-32">
+              <div className="text-4xl font-mono font-bold text-white">
+                이용내역
+              </div>
+              <div className="w-full h-[70%] flex justify-center items-center">
+                이용내역 나올 자리
+              </div>
+            </div>
+          )}
+          {stat === 3 && <div>123</div>}
+        </div>
+      </div>
+    </div>
+  );
+}
