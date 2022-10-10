5456e481150cdab984eee7d1188dbf211b30ec23
ljs <qazplm807733@gmail.com>
2022-10-10 21:53:52 +0900

    bug fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,12 +26,14 @@ const Home: NextPage = () => {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
       {
+        type: "line",
         label: "Dataset 1",
         borderColor: "rgb(54, 162, 235)",
         borderWidth: 2,
         data: [3, 4, 7, 8, 1],
       },
       {
+        type: "line",
         label: "Dataset 2",
         backgroundColor: "rgb(255, 99, 132)",
         data: [1, 2, 3, 4, 5, 8],
@@ -39,6 +41,7 @@ const Home: NextPage = () => {
         borderWidth: 2,
       },
       {
+        type: "line",
         label: "Dataset 3",
         backgroundColor: "rgb(75, 192, 192)",
         data: [1, 2, 3, 4, 5],
