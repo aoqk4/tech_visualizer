76cfbc06df8b4aeb6864a3130579e508a67141c7
ljs <qazplm807733@gmail.com>
2022-10-12 20:48:15 +0900

    add hassome

--- pages/api/techmarketinfo/index.ts
+++ pages/api/techmarketinfo/index.ts
@@ -55,14 +55,8 @@ export default async function handler(
     try {
       const tsearchData1 = await prisma.techMarketInfo.findMany({
         where: {
-          kwrdDtl: req.body || {
-            isEmpty: false,
-            has: req.body,
-          },
-          OR: {
-            kwrdDtl: {
-              has: req.body,
-            },
+          kwrdDtl: {
+            hasSome: [req.body],
           },
         },
         select: {
